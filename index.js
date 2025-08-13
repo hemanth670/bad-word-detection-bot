d623bc635c5c99497e19d55cf3aa70bbbfcf2a65
BINGI HEMANTH KUMAR <hemanthbingi143@gmail.com>
2025-08-13 12:08:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+let badWords = require("./list-bad-word.json")
+let msgs = require("./list-msg.json")
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  try {
+    let chatClient = msg.content.toLocaleLowerCase()
+    chatClient = chatClient.split(' ')
+    for (let index = 0; index < chatClient.length; index++) {
+      const element = chatClient[index];
+
+      if (badWords.id.includes(element)) {
+        msg.reply(msgs.id[Math.floor(Math.random() * msgs.id.length)])
+        break;
+      }
+
+      if (badWords.en.includes(element.toLocaleLowerCase())) {
+        msg.reply(msgs.en[Math.floor(Math.random() * msgs.en.length)])
+        break;
+      }
+
+    }
+
+
+  } catch (err) {
+    msg.channel.send("aku error om. msg : " + err)
+  }
+
+});
+
+client.on("error", (err) => {
+  console.error(err)
+})
+
+client.login('');
